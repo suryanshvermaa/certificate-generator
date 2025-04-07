d02c10b21e60ec5597484ed03d5b8e6fa124723f
suryanshvermaa <154125921+suryanshvermaa@users.noreply.github.com>
2025-04-08 03:38:31 +0530

    updated readme

--- server.js
+++ server.js
@@ -295,7 +295,7 @@ app.post('/admin/logout', (req, res) => {
     res.redirect('/admin/login');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 }); 
